a3fd2da50f0ce22bc50d4e4dfd6dd2b552d2f1cd
Dmitriy Fomin <79067773397@yandex.ru>
2021-08-10 00:59:05 +0300

    base template

---
+++ nyaka-shop/src/main/java/ru/fomin/nyakashop/NyakaShopApplication.java
@@ -0,0 +1,13 @@
+package ru.fomin.nyakashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyakaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyakaShopApplication.class, args);
+	}
+
+}
---
+++ nyaka-shop/src/test/java/ru/fomin/nyakashop/NyakaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fomin.nyakashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyakaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
